ab24cd8e8008fd766cdb282826dc5d0b3fc2c889
kane_he@miyapps.com <kanehe>
2013-09-12 23:10:06 +0800

    test

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-//app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public'1)));
 app.use(express.static(path.join(__dirname, 'webapp')));
 
 // development only
